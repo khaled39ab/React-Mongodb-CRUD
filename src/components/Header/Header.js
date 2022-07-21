921243ba9508088b40c4a718b8e02756827176e1
khaled39ab <khaled39ab@gmail.com>
2022-07-21 23:36:51 +0600

    created simple navbar

---
+++ src/components/Header/Header.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { NavLink } from "react-router-dom";
+
+const Header = () => {
+    return (
+        <nav>
+            <ul style={{ display: 'flex', fontSize: '20px' }}>
+                <li style={{ marginRight: '40px' }}>
+                    <NavLink to="home" > Home </NavLink>
+                </li>
+                <li style={{ marginRight: '40px' }}>
+                    <NavLink to="add-user" > Add User </NavLink>
+                </li>
+                <li style={{ marginRight: '20px' }}>
+                    <NavLink to="about-me"> About Me </NavLink>
+                </li>
+            </ul>
+        </nav>
+    );
+};
+
+export default Header;
