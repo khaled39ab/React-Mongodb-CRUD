9277c303d510fd64c11126c3aa7e6e9179cb7bf2
khaled39ab <khaled39ab@gmail.com>
2022-07-22 00:22:28 +0600

    add an user to banckend

--- src/components/AddUser/AddUser.js
+++ src/components/AddUser/AddUser.js
@@ -1,13 +1,20 @@
 import React from 'react';
 
 const AddUser = () => {
+    const handleAddUser = e =>{
+        e.preventDefault();
+        const name = e.target.name.value;
+        const email = e.target.email.value;
+
+        const user = {name, email}
+    }
     return (
         <div>
             <h2>Please Add a New User</h2>
-            <form>
-                <input type="text" name='username' placeholder='Username' />
+            <form onSubmit={handleAddUser}>
+                <input type="text" name='name' placeholder='Name' required/>
                 <br />
-                <input type="password" name='password' placeholder='Password' />
+                <input type="email" name='email' placeholder='Email' required/>
                 <br />
                 <button>Add User</button>
             </form>
