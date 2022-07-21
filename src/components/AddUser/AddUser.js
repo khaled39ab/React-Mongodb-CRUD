1ac25b1c3e9651ae0173f6ec05bbd9c88f19ef51
khaled39ab <khaled39ab@gmail.com>
2022-07-21 23:41:03 +0600

    created simple add user page

--- src/components/AddUser/AddUser.js
+++ src/components/AddUser/AddUser.js
@@ -4,6 +4,13 @@ const AddUser = () => {
     return (
         <div>
             <h2>Please Add a New User</h2>
+            <form>
+                <input type="text" name='username' placeholder='Username' />
+                <br />
+                <input type="password" name='password' placeholder='Password' />
+                <br />
+                <button>Add User</button>
+            </form>
         </div>
     );
 };
