14002561795f328154c2be8ae5c6dd0d11b5536b
khaled39ab <khaled39ab@gmail.com>
2022-07-22 23:15:16 +0600

    send data to the server

--- src/components/AddUser/AddUser.js
+++ src/components/AddUser/AddUser.js
@@ -1,20 +1,33 @@
 import React from 'react';
 
 const AddUser = () => {
-    const handleAddUser = e =>{
+    const handleAddUser = e => {
         e.preventDefault();
         const name = e.target.name.value;
         const email = e.target.email.value;
 
-        const user = {name, email}
+        const user = { name, email }
+
+        //sent data to the server
+        fetch('http://localhost:5000/user', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(user)
+        })
+            .then(res => res.json())
+            .then(data => console.log(data))
     }
+
+    
     return (
         <div>
             <h2>Please Add a New User</h2>
             <form onSubmit={handleAddUser}>
-                <input type="text" name='name' placeholder='Name' required/>
+                <input type="text" name='name' placeholder='Name' required />
                 <br />
-                <input type="email" name='email' placeholder='Email' required/>
+                <input type="email" name='email' placeholder='Email' required />
                 <br />
                 <button>Add User</button>
             </form>
