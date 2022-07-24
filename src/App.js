aee9ace190ba085b94d0fc21b632c98c17f4e7f3
khaled39ab <khaled39ab@gmail.com>
2022-07-25 01:30:12 +0600

    update a user from server

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import AddUser from './components/AddUser/AddUser';
 import Header from './components/Header/Header';
 import Home from './components/Home/Home';
+import UpdateUser from './components/UpdateUser/UpdateUser';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <Route path='/' element={<Home></Home>}></Route>
         <Route path='/home' element={<Home></Home>}></Route>
         <Route path='/add-user' element={<AddUser></AddUser>}></Route>
+        <Route path='/update/:id' element={<UpdateUser></UpdateUser>}></Route>
       </Routes>
     </div>
   );
---
+++ src/components/UpdateUser/UpdateUser.js
@@ -0,0 +1,51 @@
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom'
+
+const UpdateUser = () => {
+    const { id } = useParams();
+    const [user, setUser] = useState({});
+    useEffect(() => {
+        const url = `http://localhost:5000/update/${id}`;
+        fetch(url)
+            .then(res => res.json())
+            .then(data => setUser(data))
+    }, []);
+
+    const handleUpdateUser = e => {
+        e.preventDefault();
+        const name = e.target.name.value;
+        const email = e.target.email.value;
+
+        const user = { name, email }
+
+        //sent data to the server
+        const url = `http://localhost:5000/update/${id}`;
+        fetch(url, {
+            method: 'PUT',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(user)
+        })
+            .then(res => res.json())
+            .then(data => {
+                alert('User Updated successfully...')
+                e.target.reset();
+            });
+    }
+
+    return (
+        <div>
+            <h2>Updating User: {user.name}</h2>
+            <form onSubmit={handleUpdateUser}>
+                <input type="text" name='name' placeholder='Name' required/>
+                <br />
+                <input type="email" name='email' placeholder='Email' required />
+                <br />
+                <button>Update</button>
+            </form>
+        </div>
+    );
+};
+
+export default UpdateUser;
