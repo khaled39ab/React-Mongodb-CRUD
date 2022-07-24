f689525b459015cdfb4252c1a9facfa8f2110721
khaled39ab <khaled39ab@gmail.com>
2022-07-24 23:33:35 +0600

    create delete button and handle it

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,9 +1,32 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 const Home = () => {
+    const [users, setUsers] = useState([]);
+    useEffect(() => {
+        fetch('http://localhost:5000/user')
+            .then(res => res.json())
+            .then(data => setUsers(data))
+    }, []);
+
+    const handleDeleteUser = id => {
+        const proceed = window.confirm('Want to delete?')
+        if (proceed) {
+            console.log('deleting ', id);
+        }
+    }
     return (
         <div>
-            <h2>This is home</h2>
+            <h1>This is home</h1>
+            <h2>Available Users: {users.length}</h2>
+            <ul>
+                {
+                    users.map(user => <li
+                        key={user._id}
+                    >Name:{user.name} :: Email: {user.email}
+                        <button onClick={() => handleDeleteUser(user._id)}>x</button>
+                    </li>)
+                }
+            </ul>
         </div>
     );
 };
