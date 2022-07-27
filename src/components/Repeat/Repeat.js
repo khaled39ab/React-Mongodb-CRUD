d8c9730f2ea8d8dc700e6319fd2f65fb2a2e4ec6
khaled39ab <khaled39ab@gmail.com>
2022-07-27 21:53:25 +0600

    repeat send data to server

--- src/components/Repeat/Repeat.js
+++ src/components/Repeat/Repeat.js
@@ -6,7 +6,19 @@ const Repeat = () => {
         const name = e.target.name.value;
         const email = e.target.email.value;
 
-        const user = { name, email }
+        const user = { name, email };
+
+        fetch('http://localhost:5000/users', {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(user)
+        })
+            .then(res => res.json())
+            .then(data => {
+                console.log('success', data);
+            })
     }
     return (
         <div>
